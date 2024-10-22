d627282d6cf6cade0ae4abd63ede1f7d5d4899e8
AlekseiNime <mamagold1213@gmail.com>
2024-10-22 21:37:24 +0300

    Практикум №4

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -10,41 +10,81 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
+            //Task_1 -------------------------------------------------------------
 
-            Console.Write("Сколько вы хотите сложить чисел? (2 или 3)?: ");
-            int a = int.Parse(Console.ReadLine());
+            //Console.Write("Сколько вы хотите сложить чисел? (2 или 3)?: ");
+            //int a = int.Parse(Console.ReadLine());
 
-            if (a != 2 && a != 3)
-            {
-                Console.Write("Неверное значение.");
-                Console.ReadKey();
-            }
+            //if (a != 2 && a != 3)
+            //{
+            //    Console.Write("Неверное значение.");
+            //    Console.ReadKey();
+            //}
 
-            if (a == 2)
-            {
-                Console.WriteLine("Введите два числа для суммы:");
-                Console.Write("Первое число:");
-                double num1 = Convert.ToDouble(Console.ReadLine());
-                Console.Write("Второе число:");
-                double num2 = Convert.ToDouble(Console.ReadLine());
-                double sum2 = num1 + num2;
-                Console.WriteLine($"Сумма двух чисел: {sum2}");
-                Console.ReadKey();
-            }
+            //if (a == 2)
+            //{
+            //    Console.WriteLine("Введите два числа для суммы:");
+            //    Console.Write("Первое число:");
+            //    double num1 = Convert.ToDouble(Console.ReadLine());
+            //    Console.Write("Второе число:");
+            //    double num2 = Convert.ToDouble(Console.ReadLine());
+            //    double sum2 = num1 + num2;
+            //    Console.WriteLine($"Сумма двух чисел: {sum2}");
+            //    Console.ReadKey();
+            //}
 
-            if (a == 3)
-            {
-                Console.WriteLine("Введите три числа для суммы:");
-                Console.Write("Первое число:");
-                double num3 = Convert.ToDouble(Console.ReadLine());
-                Console.Write("Второе число:");
-                double num4 = Convert.ToDouble(Console.ReadLine());
-                Console.Write("Третье число:");
-                double num5 = Convert.ToDouble(Console.ReadLine());
-                double sum3 = num3 + num4 + num5;
-                Console.WriteLine($"Сумма трех чисел: {sum3}");
-                Console.ReadKey();
-            }
+            //if (a == 3)
+            //{
+            //    Console.WriteLine("Введите три числа для суммы:");
+            //    Console.Write("Первое число:");
+            //    double num3 = Convert.ToDouble(Console.ReadLine());
+            //    Console.Write("Второе число:");
+            //    double num4 = Convert.ToDouble(Console.ReadLine());
+            //    Console.Write("Третье число:");
+            //    double num5 = Convert.ToDouble(Console.ReadLine());
+            //    double sum3 = num3 + num4 + num5;
+            //    Console.WriteLine($"Сумма трех чисел: {sum3}");
+            //    Console.ReadKey();
+            //}
+
+            //Task_2 --------------------------------------------------------------
+
+            //Console.WriteLine("Введите длину стороны 1: ");
+            //double side1 = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine("Введите длину стороны 2: ");
+            //double side2 = Convert.ToDouble(Console.ReadLine());
+
+            //double perimeter = 2 * (side1 + side2);
+            //double area = side1 * side2;
+
+            //Console.WriteLine($"Периметр: {perimeter}");
+            //Console.WriteLine($"Площадь: {area}");
+
+            //Console.ReadKey();
+
+            //Task_3 --------------------------------------------------------------
+
+            //Console.WriteLine("Введите длину первого катета:");
+            //double katet1 = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine("Введите длину второго катета:");
+            //double katet2 = Convert.ToDouble(Console.ReadLine());
+
+            //double gip = Math.Sqrt(katet1 * katet1 + katet2 * katet2);
+
+            //Console.WriteLine($"Длина гипотенузы: {gip}");
+
+            //Console.ReadKey();
+
+            //Task_4 --------------------------------------------------------------
+
+            //Console.Write("Введите длину стороны квадрата: ");
+            //double side = Convert.ToDouble(Console.ReadLine());
+            //double perimeter = 4 * side;
+            //double area = side * side;
+            //Console.WriteLine($"Для стороны {side}: Периметр = {perimeter}, Площадь = {area}");
+            //Console.ReadKey();
         }
     }
 }
