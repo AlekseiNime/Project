749deadae4fcd182a4fcfde341edf5e00f04524a
AlekseiNime <mamagold1213@gmail.com>
2024-10-28 19:53:43 +0300

    Практикум 4

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task1
+            //Console.WriteLine("Введите первое число:");
+            //double num1 = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine("Введите второе число:");
+            //double num2 = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine("Введите третье число:");
+            //double num3 = Convert.ToDouble(Console.ReadLine());
+
+            //double sum = num1 + num2 + num3;
+
+            //Console.WriteLine($"Сумма {num1}, {num2} и {num3} равна {sum}");
+
+            //Console.ReadKey();
+
+
+
+
+
+
+        }
+    }
+}
