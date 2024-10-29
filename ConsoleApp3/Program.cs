2542c9a1ad3c7964e5fb2665b546fc6500718936
AlekseiNime <mamagold1213@gmail.com>
2024-10-29 09:50:35 +0200

    Class

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task_1-----------------------------
+            //int a, b, c;
+            //Console.Write("a= ");
+            //a = int.Parse(Console.ReadLine());
+            //Console.Write("b= ");
+            //b = int.Parse(Console.ReadLine());
+            //c = a + b;
+            //Console.WriteLine($"{a}+{b}={c}");
+            //Console.ReadKey();
+            //Task_2-----------------------------
+            //int a, b, c;
+            //Console.Write("a= ");
+            //a = int.Parse(Console.ReadLine());
+            //Console.Write("b= ");
+            //b = int.Parse(Console.ReadLine());
+            //c = a + b;
+            //Console.WriteLine($"{a}+{b}={b}+{a}");
+            //Console.ReadKey();
+            //Task_3----------------------------
+            //double a, b, c;
+            //Console.Write("a= ");
+            //a = double.Parse(Console.ReadLine());
+            //Console.Write("b= ");
+            //b = double.Parse(Console.ReadLine());
+            //c = a * b;
+            //Console.WriteLine($"{a:f1}*{b}={c:f1}");
+            //Console.ReadKey();
+            //Task_4----------------------------
+            //double a, b, c;
+            //Console.Write("a= ");
+            //a = double.Parse(Console.ReadLine());
+            //Console.Write("b= ");
+            //b = double.Parse(Console.ReadLine());
+            //Console.Write("c= ");
+            //c = double.Parse(Console.ReadLine());
+
+            //Console.WriteLine($"<{a:f2}+{b:f2}>+{c:f2}={a:f2}+<{b:f2}+{c:f2}>");
+            //Console.ReadKey();
+            //Task_5
+
+        }
+    }
+}
