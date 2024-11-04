c352bf04abd8cb03dc3e5e0f0e9f7b87844ec4c1
AlekseiNime <mamagold1213@gmail.com>
2024-11-04 21:33:55 +0200

    Практикум №6

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -45,11 +45,67 @@ namespace ConsoleApp3
             //b = double.Parse(Console.ReadLine());
             //Console.Write("c= ");
             //c = double.Parse(Console.ReadLine());
-
             //Console.WriteLine($"<{a:f2}+{b:f2}>+{c:f2}={a:f2}+<{b:f2}+{c:f2}>");
             //Console.ReadKey();
-            //Task_5
-
+            //Task_5----------------------------
+            //Console.Write("a=  ");
+            //double a = Convert.ToDouble(Console.ReadLine());
+            //Console.Write("b=  ");
+            //double b = Convert.ToDouble(Console.ReadLine());
+            //Console.Write("c=  ");
+            //double c = Convert.ToDouble(Console.ReadLine());
+            //Console.Write("d=  ");
+            //double d = Convert.ToDouble(Console.ReadLine());
+            //double e = (a/b) + (c/d);
+            //Console.WriteLine($"{a:f2}/{b:f2}+{c:f2}/{d:f2}={e:f2}");
+            //Console.ReadKey();
+            //Task_6----------------------------
+            //Console.Write("a=");
+            //int a = int.Parse(Console.ReadLine());
+            //Console.Write("b=");
+            //int b = int.Parse(Console.ReadLine());
+            //int c = a + b;
+            //int d = a - b;
+            //int e = a * b;
+            //Console.WriteLine($"a+b={a}+{b}={c}    a-b={a}-{b}={d}   a*b={a}*{b}={e}");
+            //Console.ReadKey();
+            //Task_7----------------------------
+            //Console.Write("a=");
+            //int a = int.Parse(Console.ReadLine());
+            //Console.Write("b=");
+            //int b = int.Parse(Console.ReadLine());
+            //Console.Write("c=");
+            //int c = int.Parse(Console.ReadLine());
+            //int d = (b + c) * a;
+            //Console.WriteLine($"[a*(b+c)]=[(b+c)*a]: [{a}*({b}+{c})]=[({b}+{c})*{a}]={d}");
+            //Console.ReadKey();
+            //Task_8----------------------------
+            //Console.Write("a=  ");
+            //double a = Convert.ToDouble(Console.ReadLine());
+            //Console.Write("b=  ");
+            //double b = Convert.ToDouble(Console.ReadLine());
+            //Console.Write("c=  ");
+            //double c = Convert.ToDouble(Console.ReadLine());
+            //double d = a + b + c;
+            //double e = Math.Round(b, 2);
+            //Console.WriteLine($"({a:f4}+({e:f4}+{c:f4}))=({a:f4}+{c:f4}+{e:f4})");
+            //Console.ReadKey();
+            //Task_9----------------------------
+            //Console.Write("x=");
+            //double x = Convert.ToDouble(Console.ReadLine());
+            //Console.Write("y=");
+            //double y = Convert.ToDouble(Console.ReadLine());
+            //double z = x * 3;
+            //Console.WriteLine($"округленный с точностью до 4 знаков результат: {z}/{y}={z/y:f4}");
+            //Console.ReadKey();
+            //Task_10----------------------------
+            //Console.Write("a=");
+            //double a = Convert.ToDouble(Console.ReadLine());
+            //Console.Write("b=");
+            //double b = Convert.ToDouble(Console.ReadLine());
+            //double c = a * b;
+            //Console.WriteLine($"a*b={Math.Round(a, 3):f3}*{Math.Round(b, 3):f3}={Math.Round(c, 3)}   a*b={a:f5}*{b:f5}={c:f5}");
+            //Console.ReadKey();
         }
     }
 }
